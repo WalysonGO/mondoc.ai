99594f077c1c7eebf901710fc42aec7599cf94aa
WalysonGO <walysonoliveira143@gmail.com>
2025-02-24 23:44:35 -0300

    docs: att docs - install.sh with update

--- install.sh
+++ install.sh
@@ -8,6 +8,21 @@ check_command_success() {
     fi
 }
 
+# Função para atualizar o Mondoc
+update_mondoc() {
+    echo "Atualizando o Mondoc..."
+    cd /tmp
+    git clone https://github.com/WalysonGO/mondoc.ai.git
+    check_command_success
+    cd mondoc.ai
+    chmod +x mondoc
+    sudo mv mondoc /usr/local/bin/mondoc
+    check_command_success
+    cd ..
+    rm -rf mondoc.ai
+    echo "Mondoc atualizado com sucesso!"
+}
+
 # Passo 1: Clonar o repositório
 echo "Baixando o repositório do GitHub..."
 git clone https://github.com/WalysonGO/mondoc.ai.git
@@ -21,21 +36,17 @@ echo "Concedendo permissão de execução ao binário..."
 chmod +x mondoc
 check_command_success
 
-# Passo 4: (Opcional) Mover o binário para o diretório de execução global
-echo "Deseja mover o binário para o diretório /usr/local/bin? (S/N)"
-read -r move_binário
-if [[ "$move_binário" == "S" || "$move_binário" == "s" ]]; then
-    echo "Movendo o binário para /usr/local/bin..."
-    sudo mv mondoc /usr/local/bin/mondoc
-    check_command_success
-fi
+# Passo 4: Mover o binário para o diretório de execução global
+echo "Movendo o binário para /usr/local/bin..."
+sudo mv mondoc /usr/local/bin/mondoc
+check_command_success
 
 # Passo 5: Execução do binário
 echo "Deseja rodar o Mondoc agora? (S/N)"
 read -r rodar_mondoc
 if [[ "$rodar_mondoc" == "S" || "$rodar_mondoc" == "s" ]]; then
     echo "Executando o Mondoc..."
-    ./mondoc
+    mondoc
 fi
 
 echo "Instalação e configuração do Mondoc.ai concluída com sucesso!"
